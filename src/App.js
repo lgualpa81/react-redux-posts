de8f2a90b5050acb58a61e72e935c4ce65d2f7c6
Leonardo Gualpa <lgualpa81@gmail.com>
2022-08-06 11:35:19 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
-function App() {
+import { Menu } from "./components/Menu";
+import { PaginaInicio } from "./pages/PaginaInicio";
+import PaginaPosts from "./pages/PaginaPosts";
+import PaginaPost from "./pages/PaginaPost";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Menu />
+      <Switch>
+        <Route exact path="/" component={PaginaInicio} />
+        <Route exact path="/posts" component={PaginaPosts} />
+        <Route exact path="/posts/:postId" component={PaginaPost} />
+        <Redirect to="/" />
+      </Switch>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
+
+//https://www.neoguias.com/tutorial-redux/
